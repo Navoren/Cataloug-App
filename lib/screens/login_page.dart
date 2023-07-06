d1330a6137d64e598db41df4218ea69f80405fab
navoren <namantomar1453@gmail.com>
2023-07-06 16:57:53 +0530

    Added Animation in Login button

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:secondapp/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changedButton = false;
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +24,9 @@ class LoginPage extends StatelessWidget {
               "assets/images/undraw_Mobile_login_re_9ntv.png",
               fit: BoxFit.cover,
             ),
-            const Text(
-              "Welcome",
-              style: TextStyle(
+            Text(
+              "Welcome $name",
+              style: const TextStyle(
                 fontSize: 40,
                 fontWeight: FontWeight.bold,
               ),
@@ -42,6 +50,10 @@ class LoginPage extends StatelessWidget {
                         hintText: "Enter Username",
                         labelText: "Username",
                       ),
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
                     TextFormField(
                       obscureText: true,
@@ -53,20 +65,51 @@ class LoginPage extends StatelessWidget {
                     const SizedBox(
                       height: 20.0,
                     ),
-                    SizedBox(
-                      width: 100,
-                      height: 45,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(30))),
-                        onPressed: () {
-                          Navigator.pushNamed(context, MyRoutes.homeRoute);
-                        },
-                        child: const Text("Login"),
+
+                    InkWell(
+                      onTap: () async {
+                        setState(() {
+                          changedButton = true;
+                        });
+                        await Future.delayed(const Duration(milliseconds: 750));
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 750),
+                        width: changedButton ? 50 : 100,
+                        height: 50,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius:
+                                BorderRadius.circular(changedButton ? 50 : 8)),
+                        child: changedButton
+                            ? const Icon(
+                                Icons.done,
+                                color: Colors.white,
+                              )
+                            : const Text(
+                                "Login",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
                       ),
                     )
+                    // SizedBox(
+                    //   width: 100,
+                    //   height: 45,
+                    //   child: ElevatedButton(
+                    //     style: ElevatedButton.styleFrom(
+                    //         elevation: 3,
+                    //         shape: RoundedRectangleBorder(
+                    //             borderRadius: BorderRadius.circular(30))),
+                    //     onPressed: () {
+                    //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    //     },
+                    //     child: const Text("Login"),
+                    //   ),
+                    // )
                   ],
                 ))
           ],
